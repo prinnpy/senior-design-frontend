f29a702dc426ed2e667057eb9df8ff1672e080ef
Marcus Gallegos <marcus.gallegos@ucdenver.edu>
2021-03-15 22:58:43 -0600

    add build

---
+++ build/precache-manifest.21d9afeb0f6a25d215bde436f2c29b9e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0976dd26e9880791d5dd3bbd87ee620",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6e2a71a1434b708d23e",
+    "url": "/static/css/2.87a9c7a2.chunk.css"
+  },
+  {
+    "revision": "e7c29c5fa5b4ee996b12",
+    "url": "/static/css/main.b896bd20.chunk.css"
+  },
+  {
+    "revision": "d6e2a71a1434b708d23e",
+    "url": "/static/js/2.97be4226.chunk.js"
+  },
+  {
+    "revision": "70ff94f616fbfa87ae91735f02c566ad",
+    "url": "/static/js/2.97be4226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7c29c5fa5b4ee996b12",
+    "url": "/static/js/main.720c868c.chunk.js"
+  },
+  {
+    "revision": "26e01c5726ca5059e1bf",
+    "url": "/static/js/runtime-main.a34abd03.js"
+  },
+  {
+    "revision": "968bf140ed769b00663b79a12b641e22",
+    "url": "/static/media/codeBg.968bf140.jpg"
+  }
+]);
